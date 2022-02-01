8cb44e0df5adefc4cc2894896fbf0d808a2b42f4
plitche <kysu728@naver.com>
2022-02-01 19:38:48 +0900

    15. 단방향 연관관계

---
+++ Project/ex1-hello-jpa/src/main/java/hellojpa/JpaMain4.java
@@ -0,0 +1,47 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain4 {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf1 = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf1.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            //저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member2 member2 = new Member2();
+            member2.setUsername("member1");
+//            member2.setTeamId(team.getId());
+            member2.setTeam(team); // jpa가 알아서 pk값을 꺼내서 fk값을 사용한다.
+
+            em.persist(member2);
+            
+            em.flush(); // 영속성 컨텍스트 비우기
+            em.close(); // 초기화
+
+            Member2 findMember = em.find(Member2.class, member2.getId());
+//            Long findTeamId = findMember.getTeamId();
+//            Team findTeam = em.find(Team.class, findTeamId);
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam.getName());
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf1.close();
+    }
+}
---
+++ Project/ex1-hello-jpa/src/main/java/hellojpa/Member2.java
@@ -0,0 +1,46 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+public class Member2 {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name="USERNAME")
+    private String username;
+
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    // 이 둘의 관계가 뭔지 jpa에게 알려줘야 한다.
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
