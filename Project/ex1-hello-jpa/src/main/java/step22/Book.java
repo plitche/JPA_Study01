09f069eb1290692e9c4fa0995f74314841d856e3
plitche <kysu728@naver.com>
2022-02-18 22:54:14 +0900

    22. 고급 매핑 : 조인 전략

---
+++ Project/ex1-hello-jpa/src/main/java/step22/Book.java
@@ -0,0 +1,12 @@
+package step22;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("B")
+public class Book extends Item {
+
+    private String author;
+    private String isbn;
+}
