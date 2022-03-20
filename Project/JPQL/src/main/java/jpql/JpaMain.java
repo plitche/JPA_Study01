054e4fa65995812b353acb5f6da2f4a00d68740e
plitche <kysu728@naver.com>
2022-03-20 18:49:59 +0900

    37. 객체지향 쿼리 언어1(기본 문법) - 페이지

--- Project/JPQL/src/main/java/jpql/JpaMain.java
+++ Project/JPQL/src/main/java/jpql/JpaMain.java
@@ -18,78 +18,25 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
-
-            // 반환 타입이 명확할 때 TypedQuery, 명확하지 않을 때 Query
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-            Query query3 = em.createQuery("select m.username, m.age from Member m");
-
-            List<Member> resultList = query1.getResultList();
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
+            for (int i=0; i<100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
             }
-
-            TypedQuery<Member> query4 = em.createQuery("select m from Member m where m.id = 10", Member.class);
-
-            Member singleResult = query4.getSingleResult();
-            System.out.println("singleResult = " + singleResult);
-
-//            TypedQuery<Member> query5 = em.createQuery("select m from Member m where m.username = :username", Member.class);
-//            query5.setParameter("username", "member1");
-//            Member singleResult2 = query5.getSingleResult();
-//            System.out.println("singleResult2 = " + singleResult2.getUsername());
 
             em.flush();
             em.clear();
 
-            // 이 Member Entity들은 영속성 컨텍스트들에 의해 관리가 될까 안될까 ?
-            // Entity 프로젝션을 하면 select 절에 의한 결과가 모두 관리 된다.
-            List<Member> result = em.createQuery("select m from Member m", Member.class)
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
                     .getResultList();
 
-            Member findMember = result.get(0);
-            findMember.setAge(20);
-
-            // join query가 나간다.
-            List<Team> result2 = em.createQuery("select m.team from Member m", Team.class)
-                    .getResultList();
-            // 위 아래는 같은 jpql
-            em.createQuery("select t from Member m join m.team t", Team.class).getResultList();
-
-            // 임베디드 타입 프로젝션
-            em.createQuery("select o.address from Order o", Address.class)
-                    .getResultList();
-
-            // 스칼라 타입 프로젝션
-            List resultList1 = em.createQuery("select distinct m.username, m.age from Member m")
-                    .getResultList();
-
-            // 여러 값 가져오기
-            // 1
-            Object o = resultList1.get(0);
-            Object[] result3 = (Object[]) o;
-            System.out.println("username = " + result3[0]);
-            System.out.println("age = " + result3[1]);
-
-            // 2
-            List<Object[]> resultList2 = em.createQuery("select distinct m.username, m.age from Member m")
-                    .getResultList();
-
-            Object[] result4 = resultList2.get(0);
-            System.out.println("username = " + result4[0]);
-            System.out.println("age = " + result4[1]);
-
-            // 3 (권장)
-            List<MemberDTO> resultList3 = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
-                    .getResultList();
-
-            MemberDTO memberDTO = resultList3.get(0);
-            System.out.println("username = " + memberDTO.getUsername());
-            System.out.println("age = " + memberDTO.getAge());
+            System.out.println("result.size = " + result.size());
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
