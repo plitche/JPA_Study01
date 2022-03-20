c0414cc1ce167c489e20b1906341480fdc6e6417
plitche <kysu728@naver.com>
2022-03-20 15:29:47 +0900

    34. 객체지향 쿼리 언어1(기본 문법) - JPQL

---
+++ Project/ex1-hello-jpa/src/main/java/step34/intro/JpaMain.java
@@ -0,0 +1,41 @@
+package step34.intro;
+
+import antlr.collections.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            // Member 객체 자체를 조회해 와 : m
+            List<Member> result = em.createQuery(
+                    "select m From Member as m where m.username like '%kim%'",
+                    Member.class
+            ).getResultList();
+
+            for (Member member : result) {
+                System.out.println("member = " + member);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+}
