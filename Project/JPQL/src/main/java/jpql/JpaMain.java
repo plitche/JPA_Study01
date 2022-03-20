dc120e034b9e10229f1aa4e0b2e8b14131a2d5b9
plitche <kysu728@naver.com>
2022-03-20 19:08:05 +0900

    38. 객체지향 쿼리 언어1(기본 문법) - 조인

--- Project/JPQL/src/main/java/jpql/JpaMain.java
+++ Project/JPQL/src/main/java/jpql/JpaMain.java
@@ -18,26 +18,44 @@ public class JpaMain {
 
         try {
 
-            for (int i=0; i<100; i++) {
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member");
+            member.setAge(10);
+            member.setTeam(team);
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
+            // inner 는 생략 가능
+            String query = "select m from Member m inner join m.team t";
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
 
-            System.out.println("result.size = " + result.size());
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
-            }
+            // outer는 생략 가능
+            String query1 = "select m from Member m left outer join m.team t";
+            List<Member> result1 = em.createQuery(query1, Member.class)
+                    .getResultList();
 
+            // 세타 조인
+            String query2 = "select m from Member m, Team t where m.username = t.name";
+            List<Member> result2 = em.createQuery(query2, Member.class)
+                    .getResultList();
+            System.out.println("result2.size = " + result2.size());
+
+
+            String query3 = "select m from Member m left join m.team t on t.name = 'teamA'";
+            List<Member> result3 = em.createQuery(query3, Member.class)
+                    .getResultList();
+
+            String query4 = "select m from Member m left join Team t on m.username = t.name";
+            List<Member> result4 = em.createQuery(query4, Member.class)
+                    .getResultList();
+            
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
